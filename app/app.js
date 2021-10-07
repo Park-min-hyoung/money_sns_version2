da85ef208dbc6db38f5051e2523961a3727f5bb7
Park-min-hyoung <qkralswgud1324@naver.com>
2021-10-07 21:16:25 +0900

    Port 수정

--- app/app.js
+++ app/app.js
@@ -3,8 +3,8 @@
 // 모듈
 const express = require("express");
 const app = express();
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -17,17 +17,20 @@ app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/src/public`));
 
 app.use(express.json());
-app.use(express.urlencoded( {extended: true} ));
+app.use(express.urlencoded({ extended: true }));
 
-app.use(session({
-    key: 'sid',
+app.use(
+  session({
+    key: "sid",
     secret: "ksdjwv@!!ssz224455mml;/.v", // 이 값을 이용해 암호화옵션
     resave: false, // 세션이 수정되지 않아도 항상 저장할지 확인하는 옵션
     saveUninitialized: true, // 세션이 uninitalized 상태로 미리 만들어서 저장하는지 묻는 옵션
-    cookie: { // 쿠키에 들어가는 세션 ID값의 옵션
-        maxAge: 4000 * 60 * 10 // 2분후 폭파
-    }
-}));
+    cookie: {
+      // 쿠키에 들어가는 세션 ID값의 옵션
+      maxAge: 4000 * 60 * 10, // 2분후 폭파
+    },
+  })
+);
 
 app.use("/", home); // use -> 미들 웨어를 등록해주는 메소드
 app.use("/", board);
